b62baa3d8fffeee1f447cb3f538cc1b596a2e1bb
Seth.Goldin@mpfi.org <GoldinS@mpfi.org>
2021-05-18 12:32:00 -0400

    add more color schemes

--- globals.py
+++ globals.py
@@ -1,3 +1,3 @@
-PALETTE_OPS = ["rocket_r", "crest", "rocket", "mako", "flare", "viridis", "magma", "cubehelix"]
+PALETTE_OPS = ["rocket_r", "crest", "rocket", "mako", "flare", "viridis", "magma", "cubehelix", "mako_r",  "crest_r",  "flare_r", "viridis_r", "magma_r", ]
 UNIT_OPS = ['px', 'nm', 'μm', 'metric']
 PAGE_NAMES = ["Main", "NND"]#, "Foo", "Bar", "Baz", "Bop"
