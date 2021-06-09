add7a0abaa7dba44b30adedd853646ef7f8948be
Seth.Goldin@mpfi.org <GoldinS@mpfi.org>
2021-06-09 13:37:29 -0400

    clean up and comment nnd cluster file

--- workflows/nnd_clust.py
+++ workflows/nnd_clust.py
@@ -25,21 +25,19 @@ _______________________________
 @random_coordinate_list: list of randomly generated coordinates
 """
 
-
 def run_nnd_clust(df, prog, random_coordinate_list, min_clust_size=3, distance_threshold=120, n_clusters=None,
                   affinity='euclidean', linkage='ward'):
+    # remove elements of list that show up less than k times
     def minify_list(lst, k):
         counted = Counter(lst)
         return [el for el in lst if counted[el] >= k]
 
+    # find centroids in df w/ clusters
     def find_centroids(cl_df, clust):
         centroids = []
         centroid_ids = []
-        # print(set(clust))
         for c in set(clust):
-            # print(c)
             cl = cl_df.loc[cl_df['cluster_id'] == c]
-            # print(cl_df.loc[cl_df['cluster_id'] == c])
             n = 0
             x = 0
             y = 0
@@ -52,93 +50,76 @@ def run_nnd_clust(df, prog, random_coordinate_list, min_clust_size=3, distance_t
                 y /= n
                 centroids.append((round(y, 3), round(x, 3)))
                 centroid_ids.append(c)
-        # print(centroids)
         print("generated centroids")
         return centroids, centroid_ids
 
+    # cluster data
     def cluster(data, n_clust, d_threshold, min_size):
-        # print(distance_threshold, n_clusters)
+        # TODO: come up with a better way of handling this
+        # translate string var props to their real value (unfortunately necessary because of text inputs)
         if n_clust != "None":
             d_threshold = None
             n_clust = int(n_clust)
         else:
             n_clust = None
             d_threshold = int(d_threshold)
+        # actually run sklearn clustering function
         hc = AgglomerativeClustering(n_clusters=n_clust, distance_threshold=d_threshold, affinity=affinity,
                                      linkage=linkage)
         clust = hc.fit_predict(real_coordinates)
+        # append cluster ids to df
         data['cluster_id'] = clust
-
-        # random coords
+        # setup random coords
         prog.update_progress(70)
         rand_coordinates = np.array(random_coordinate_list)
         rand_cluster = hc.fit_predict(rand_coordinates)
-
+        # fill random df
         rand_df = pd.DataFrame(rand_coordinates, columns=["X", "Y"])
         rand_df['cluster_id'] = rand_cluster
-
-        # for _df in [data, rand_df]:
-        #     _id = 0
-        #     count = 0
-        #     for i in _df['cluster_id']:
-        #         if i == _id:
-        #             count += 1
-        #         else:
-        #             if count < float(min_size):
-        #                 _df = _df[_df['cluster_id'] != _id]
-        #
-        #             _id = i
-        #             count = 0
-
         print("generated clusters")
         return data, rand_df, minify_list(clust, float(min_size)), minify_list(rand_cluster, float(min_size))
 
     def nnd(coordinate_list, random_coordinate_list):
-        """ FIND DIST TO CLOSEST PARTICLE """
-
+        # finds nnd between centroids
         def distance_to_closest_particle(coord_list):
-            nndlist = []
-            # print(coord_list)
-            # coord_list_len = len(coord_list)
-            # # print(coord_list_len)
-            # for i in range(coord_list_len - 1):
-            for i in range(len(coord_list)):
+            nnd_list = []
+            for z in range(len(coord_list)):
                 # update progress bar
                 small_dist = 10000000000000000000
-                # templist = [og coord (x, y), closest coord (x, y), distance]
-                templist = [0, 0, 0]
-                particle_i = coord_list[i]
+                # temp_list = [og coord (x, y), closest coord (x, y), distance]
+                temp_list = [0, 0, 0]
+                particle_i = coord_list[z]
                 particle_if = (round(particle_i[1], 3), round(particle_i[0], 3))
-                templist[0] = particle_if
+                temp_list[0] = particle_if
                 for j in range(0, len(coord_list)):
-                    if i is not j:
+                    if z is not j:
                         particle_j = coord_list[j]
                         particle_jf = (round(particle_j[1], 3), round(particle_j[0], 3))
                         dist = ((particle_jf[0] - particle_if[0]) ** 2) + ((particle_jf[1] - particle_if[1]) ** 2)
                         dist = math.sqrt(dist)
                         if dist < small_dist:
                             small_dist = round(dist, 3)
-                            templist[1] = particle_jf
-                            templist[2] = small_dist
-                nndlist.append(templist)
-
+                            temp_list[1] = particle_jf
+                            temp_list[2] = small_dist
+                nnd_list.append(temp_list)
+            # create new clean df
             clean_df = pd.DataFrame()
-            if (len(nndlist)) > 0:
+            if (len(nnd_list)) > 0:
                 # clean and convert to df
-                d = {'NND': nndlist}
+                d = {'NND': nnd_list}
                 data = pd.DataFrame(data=d)
                 # clean up df
                 clean_df[['og_centroid', 'closest_centroid', 'dist']] = pd.DataFrame(
                     [x for x in data['NND'].tolist()])
                 print("generated nnd list")
             return clean_df
-
+        # find nnd
         cleaned_real_df = distance_to_closest_particle(coordinate_list)
         cleaned_rand_df = distance_to_closest_particle(random_coordinate_list)
-
         print("found nnd")
         return cleaned_real_df, cleaned_rand_df
 
+    """ NND BETWEEN CLUSTERS """
     print("running nearest neighbor distance between clusters")
     # turn into coordinate list
     x_coordinates = np.array(df['X'])
@@ -146,51 +127,40 @@ def run_nnd_clust(df, prog, random_coordinate_list, min_clust_size=3, distance_t
     real_coordinates = []
     for i in range(len(x_coordinates)):
         real_coordinates.append([float(y_coordinates[i]), float(x_coordinates[i])])
-
     # make numpy array
     prog.update_progress(30)
     real_coordinates = np.array(real_coordinates)
-
     # cluster
     full_real_df, full_rand_df, cluster, rand_cluster = cluster(df, n_clusters, distance_threshold, min_clust_size)
-
     # generate centroids of clusters
     real_centroids, real_clust_ids = find_centroids(full_real_df, cluster)
     rand_centroids, rand_clust_ids = find_centroids(full_rand_df, rand_cluster)
-    # print(real_centroids, rand_centroids)
+    # run nearest neighbor distance on centroids
     real_df, rand_df = nnd(real_centroids, rand_centroids)
-    # print(clean_real_df.head())
-    # print(clean_real_df, real_clust_ids)
+    # add back cluster ids to df
     real_df['cluster_id'] = real_clust_ids
     rand_df['cluster_id'] = rand_clust_ids
-    # print(clean_real_df.head())
-
+    # return dataframes with all elements, dataframe with only centroids and nnd
     return full_real_df, full_rand_df, real_df, rand_df
 
 
 def draw_nnd_clust(nnd_df, cluster_df, img, bin_counts, palette="rocket_r", input_unit=Unit.PIXEL, scalar=1, circle_c=(0, 0, 255)):
-    # draw clusters
+    # color palette
     def sea_to_rgb(color):
         color = [val * 255 for val in color]
         return color
-
-    palette = create_color_pal(n_bins=len(set(cluster_df['cluster_id'])), palette_type=palette)
-
+    # draw clusters
+    cl_palette = create_color_pal(n_bins=len(set(cluster_df['cluster_id'])), palette_type=palette)
     for idx, entry in cluster_df.iterrows():
         particle = tuple(int(scalar * x) for x in [entry['X'], entry['Y']])
-        img = cv2.circle(img, particle, 10, sea_to_rgb(palette[cluster_df['cluster_id'][idx]]), -1)
-
+        img = cv2.circle(img, particle, 10, sea_to_rgb(cl_palette[cluster_df['cluster_id'][idx]]), -1)
     # draw nnd
     count = 0
     bin_idx = 0
-    # print(nnd_df.head())
     for idx, entry in nnd_df.iterrows():
         count += 1
-        # print(idx, count)
         particle_1 = entry['og_centroid']
         particle_2 = entry['closest_centroid']
-        # scale back to drawable size
-        # print("test", input_unit == 'px')
         if input_unit == Unit.PIXEL:
             particle_1 = tuple(int(scalar * x) for x in particle_1)
             particle_2 = tuple(int(scalar * x) for x in particle_2)
@@ -200,8 +170,6 @@ def draw_nnd_clust(nnd_df, cluster_df, img, bin_counts, palette="rocket_r", inpu
         if count >= bin_counts[bin_idx] and bin_idx < len(bin_counts) - 1:
             bin_idx += 1
             count = 0
-        # print(particle_1)
         img = cv2.circle(img, particle_1, 10, circle_c, -1)
         img = cv2.line(img, particle_1, particle_2, sea_to_rgb(palette[bin_idx]), 5)
-
     return img
